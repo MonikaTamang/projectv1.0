6864d8557bd298739f717635750650b5564433f6
MonikaTamang <30798365+MonikaTamang@users.noreply.github.com>
2022-10-13 15:16:23 +0545

    Update index.js
    added js

--- index.js
+++ index.js
@@ -8,3 +8,35 @@ function changeBg() {
     }
 }
 window.addEventListener('scroll', changeBg)
+window.addEventListener('scroll', changeBg)
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "menu") {
+        x.className += " responsive";
+    } else {
+        x.className = "menu";
+    }
+}
+window.onscroll = function () { myFunction() };
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
